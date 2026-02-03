b32c49b7f079c5d6a7bf40a181fba445a77a671e
Johann Heuer <johann.f.heuer@gmail.com>
2026-02-03 20:06:07 +0100

    feat: delete most for a fresh start

--- src/lib.rs
+++ src/lib.rs
@@ -1,89 +1,8 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::sync::Arc;
 
-use futures::StreamExt;
-use tokio::sync::RwLock;
+use crate::kv::KvStore;
 
-use crate::{
-    core::{CATALOG_NS, TempestType},
-    kv::KvStore,
-};
-
-#[macro_use]
-extern crate derive_more;
-
-pub mod core;
-pub mod kv;
-pub mod query;
-
-#[derive(Debug, Clone)]
-pub struct Column {
-    pub(crate) name: String,
-    pub(crate) data_type: Vec<TempestType>,
-    //pub(crate) nullable: bool,
-    //pub(crate) default: Option<Expr>,
-}
-
-#[derive(Debug, Clone)]
-pub struct TableSchema {
-    /// Every column's name and it's type.
-    pub(crate) columns: Vec<Column>,
-    /// The columns (their indices) used to uniquely identify rows of this table.
-    pub(crate) primary_key: Vec<usize>,
-}
-
-impl TableSchema {
-    pub fn new(columns: Vec<Column>, primary_key: Vec<usize>) -> Self {
-        Self {
-            columns,
-            primary_key,
-        }
-    }
-}
-
-/// Catalog implementation. The catalog keeps track of the database metadata,
-/// such as the [`TableSchema`]s.
-///
-/// [`TableSchema`]: TableSchema
-pub struct Catalog {
-    kv: Arc<dyn KvStore>,
-    /// Maps `DB_NAME` and `TABLE_NAME` to the schema for that table.
-    cache: RwLock<BTreeMap<(String, String), TableSchema>>,
-}
-
-impl Catalog {
-    pub async fn bootstrap(kv: Arc<dyn KvStore>) -> Self {
-        let mut stream = kv.scan(
-            CATALOG_NS.to_be_bytes().to_vec(),
-            (CATALOG_NS + 1).to_be_bytes().to_vec(),
-        );
-        let cache = BTreeMap::new();
-
-        while let Some((_key, _value)) = stream.next().await {
-            todo!("implement reading of old schema");
-        }
-        drop(stream);
-
-        let cache = RwLock::new(cache);
-
-        Self { kv, cache }
-    }
-
-    pub async fn set_schema(&self, database: String, table: String, schema: TableSchema) {
-        let mut cache_guard = self.cache.write().await;
-        match cache_guard.entry((database, table)) {
-            std::collections::btree_map::Entry::Vacant(e) => {
-                e.insert(schema);
-            }
-            std::collections::btree_map::Entry::Occupied(_e) => {
-                todo!("implement schema modification")
-            }
-        }
-    }
-
-    pub async fn get_schema(&self, database: String, table: String) -> Option<TableSchema> {
-        return self.cache.read().await.get(&(database, table)).cloned();
-    }
-}
+mod kv;
 
 /// Tempest implementation. Always uses the first byte of the key for the namespace.
 ///
@@ -130,14 +49,12 @@ impl Catalog {
 /// [`Catalog`]: crate::Catalog
 pub struct Tempest {
     kv: Arc<dyn KvStore>,
-    catalog: Catalog,
 }
 
 impl Tempest {
     /// Initialize this `Tempest` instance.
     pub async fn init<KV: KvStore + 'static>(kv: KV) -> Self {
         let kv = Arc::new(kv);
-        let catalog = Catalog::bootstrap(kv.clone()).await;
-        Self { kv, catalog }
+        Self { kv }
     }
 }
